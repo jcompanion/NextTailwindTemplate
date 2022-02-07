c886eaa3bea86e432474e85b9f61b7b8fa9a03b4
jcompanion <joshua.companion@g.austincc.edu>
2022-02-07 08:36:18 -0600

    updated eslint due to babel errors

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-
 export default function Home() {
   return (
-   <div className='items-center flex w-full justify-center h-screen p-4'><h1 className='transition-all text-3xl p-4 bg-gray-200 rounded shadow-lg hover:scale-105 hover:shadow-3xl hover:bg-green-100'>Hello, code time bb. GET STARTED!</h1></div>
-  )
+    <div className="items-center flex w-full justify-center h-screen p-4">
+      <h1 className="transition-all text-3xl p-4 bg-gray-200 rounded shadow-lg hover:scale-105 hover:shadow-3xl hover:bg-green-100">
+        Hello, code time bb. GET STARTED!
+      </h1>
+    </div>
+  );
 }
